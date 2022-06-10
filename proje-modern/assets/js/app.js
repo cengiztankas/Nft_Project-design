a0fa0aeeca086910ae68ca44f57d72c071471d01
Cengiz Tankaş <cengizasd.3810@gmail.com>
2022-06-10 15:08:57 +0300

    Nft_Project_Çalışması
    HTML, CSS ve Bootstrap kütüphanesini kullanarak; Küçük bir web arayüz oluşturdum...

---
+++ proje-modern/assets/js/app.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        500:{
+            items:2
+        },
+        768:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
